b87f02e92ba9bc6412e308b389449aff3abbf4b8
Yun Wu <ywu910318@gmail.com>
2025-06-17 21:30:19 +0800

    test

--- src/ecoscope-workflows-ext-mep/tests/tasks/test_collar_voltage.py
+++ src/ecoscope-workflows-ext-mep/tests/tasks/test_collar_voltage.py
@@ -1,3 +1,6 @@
+from pathlib import Path
+
+import geopandas as gpd
 import pandas as pd
 import pytz
 from ecoscope_workflows_core.tasks.filter._filter import TimeRange
@@ -5,10 +8,14 @@ from ecoscope_workflows_ext_mep.tasks import calculate_collar_voltage
 
 
 def test_calculate_collar_voltage():
-    calculate_collar_voltage(
-        relocs=None,  # Replace with a mock or test GeoDataFrame
+    relocs_path = Path(__file__).parent.parent / "data" / "relocs.parquet"
+    relocs = gpd.read_parquet(relocs_path)
+    df = calculate_collar_voltage(
+        relocs=relocs,
         time_range=TimeRange(
             since=pd.to_datetime("2025-01-01").replace(tzinfo=pytz.UTC),
             until=pd.to_datetime("2025-01-07").replace(tzinfo=pytz.UTC),
-        ),  # Replace with a mock TimeRange object
+        )
     )
+
+    assert df is not None
