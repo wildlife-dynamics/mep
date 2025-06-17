be2ef7b337477d7de38873669da634cf5e582362
Yun Wu <ywu910318@gmail.com>
2025-06-17 21:06:19 +0800

    lint

--- src/ecoscope-workflows-ext-mep/tests/tasks/test_collar_voltage.py
+++ src/ecoscope-workflows-ext-mep/tests/tasks/test_collar_voltage.py
@@ -7,6 +7,8 @@ from ecoscope_workflows_ext_mep.tasks import calculate_collar_voltage
 def test_calculate_collar_voltage():
     calculate_collar_voltage(
         relocs=None,  # Replace with a mock or test GeoDataFrame
-        time_range=TimeRange(since=pd.to_datetime("2025-01-01").replace(tzinfo=pytz.UTC),until=pd.to_datetime("2025-01-07").replace(tzinfo=pytz.UTC)),  # Replace with a mock TimeRange object
+        time_range=TimeRange(
+            since=pd.to_datetime("2025-01-01").replace(tzinfo=pytz.UTC),
+            until=pd.to_datetime("2025-01-07").replace(tzinfo=pytz.UTC),
+        ),  # Replace with a mock TimeRange object
     )
-
