c058cd4fd3de2d4040da2187acd881681d842670
Tevin Temu <tevintemu@gmail.com>
2026-09-10 20:55:17 +0300

    Fix pre-commit errors

--- src/ecoscope-workflows-ext-mep/ecoscope_workflows_ext_mep/_version.py
+++ src/ecoscope-workflows-ext-mep/ecoscope_workflows_ext_mep/_version.py
@@ -18,7 +18,7 @@ version_tuple: tuple[int | str, ...]
 commit_id: str | None
 __commit_id__: str | None
 
-__version__ = version = '1.0.3.dev5+ga2a725b08.d20260910'
-__version_tuple__ = version_tuple = (1, 0, 3, 'dev5', 'ga2a725b08.d20260910')
+__version__ = version = "1.0.3.dev5+ga2a725b08.d20260910"
+__version_tuple__ = version_tuple = (1, 0, 3, "dev5", "ga2a725b08.d20260910")
 
 __commit_id__ = commit_id = None
